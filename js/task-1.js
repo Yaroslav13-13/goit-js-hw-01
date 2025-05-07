ed00edd2481f115782425127e02e32b148017496
Yaroslav13-13 <frilanserwork13@gmail.com>
2025-05-07 19:24:34 +0300

    hw_js_01-task-1

--- js/task-1.js
+++ js/task-1.js
@@ -1,5 +1,21 @@
-const username="Yaroslav";
-console.log(username);
+// let username = "Yaroslav";
+// username = `David Vika ${username}`;
 
+// console.log(username);
 
+// let productName = "Slava";
+// let pricePerItem = null;
+// console.log(typeof productName);
 
+// const pricePerItem = 3500;
+// const orderedQuantity = 4;
+// const totalPrice = pricePerItem * orderedQuantity;
+// console.log(totalPrice);
+
+// let age = 26;
+// age = age + 2;
+// console.log(age);
+
+const username = "Poly";
+const message = "Welcome " + username + "!!!";
+console.log(message);
